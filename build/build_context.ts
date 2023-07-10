cb072094c56fd1553b5363c80de989c0b8e1387f
Tim Sullivan <timsullivanaz@gmail.com>
2023-07-09 18:44:23 -0700

    sourcemaps

--- build/build_context.ts
+++ build/build_context.ts
@@ -1,23 +1,34 @@
-import { BuildOptions } from 'esbuild'
-import { copy } from 'esbuild-plugin-copy';
+import { BuildOptions } from "esbuild";
+import { copy } from "esbuild-plugin-copy";
 
-export const getBuildContext = (watch = false): BuildOptions => ({
-    entryPoints: ['./src/browser.ts'],
+export const getBuildContext = (dev = false): BuildOptions => {
+  let watch = false;
+  let sourcemap: BuildOptions['sourcemap'] = false;
+  if (dev) {
+    watch = true;
+    sourcemap = 'linked';
+  }
+  return {
+    entryPoints: ["./src/browser.ts"],
     bundle: true,
-    minify: true,
-    sourcemap: 'external',
-    outfile: './dist/bookshelf-adventures.js',
+    minify: false,
+    sourcemap,
+    outfile: "./dist/bookshelf-adventures.js",
     plugins: [
       copy({
-        resolveFrom: 'cwd',
-        assets: [{
-          from: ['./index.html'],
-          to: ['./dist/index.html'],
-        }, {
-          from: ['./styles.css'],
-          to: ['./dist/styles.css'],
-        }],
+        resolveFrom: "cwd",
+        assets: [
+          {
+            from: ["./index.html"],
+            to: ["./dist/index.html"],
+          },
+          {
+            from: ["./styles.css"],
+            to: ["./dist/styles.css"],
+          },
+        ],
         watch,
       }),
     ],
-  })
+  };
+};
